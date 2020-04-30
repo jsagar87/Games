630604e9776be142f60e9da3b3147a3a57c7d5c1
jsagar87 <jsagar87@gmail.com>
2020-04-30 19:16:10 +0530

    Half way through for xxx cli game

--- unreal/TripleX/TrippleX.cpp
+++ unreal/TripleX/TrippleX.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
-    std::cout << "You're the phishy guy. Trying to sneak into colleagues Workstation." << std::endl ;
-    
-    std::cout << "Enter the correct code to continue..." << std::endl ;
+
+    // This is welcome message
+    cout << "You're the phishy guy. Trying to sneak into colleagues Workstation." << endl ;
+    cout << "Enter the correct code to continue..." << endl ;
+
+    // Declare 3 number code
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB *CodeC;
+
+    cout << endl;
+    // Print CodeSum and CodeProduct to the terminal
+    cout << "+ There are 3 Numbers in the code." << endl ;
+    cout << "+ The codes add-up to: " << CodeSum  << endl ;
+    cout << "+ The codes multiply to give: " << CodeProduct << endl ;
+
+    int PlayerGuess;
+
     return 0;
 }
