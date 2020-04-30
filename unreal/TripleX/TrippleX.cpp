5e354e9ff50ee928d6180e606d412782172fbf2e
jsagar87 <jsagar87@gmail.com>
2020-04-30 09:33:55 +0530

    Initial state of games

---
+++ unreal/TripleX/TrippleX.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "You're the phishy guy. Trying to sneak into colleagues Workstation." << std::endl ;
+    
+    std::cout << "Enter the correct code to continue..." << std::endl ;
+    return 0;
+}
